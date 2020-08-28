776d80c36f2bcd4d6b5365f1ac58a78c123b6ec7
askowronski <askowronski@backstopsolutions.com>
2020-08-27 20:24:54 -0500

    Remove checks on some fields

--- src/main/java/com/eventtracker/backend/entities/Event.java
+++ src/main/java/com/eventtracker/backend/entities/Event.java
@@ -16,14 +16,12 @@ public class Event {
     private String id;
     @NotBlank(message = "Type cannot be blank.")
     private String type;
-    @NotNull(message = "Duration cannot be null")
     private Long duration;
     @NotBlank(message = "Name cannot be blank.")
     private String name;
     private String notes;
     @NotNull(message = "Start Time cannot be null")
     private Date startTime;
-    @NotNull(message = "End Time cannot be null")
     private Date endTime;
     
 	
